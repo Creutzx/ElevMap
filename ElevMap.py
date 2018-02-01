cb1cff54ec46f869733a7497bdb7ac41ae5b5fe7
CREUTZBURG Brian <bcreutz@deq.state.or.us>
2018-02-01 09:09:08 -0800

    Creates Elevation Map and saves it as a .mxd file

---
+++ ElevMap.py
@@ -0,0 +1,13 @@
+import arcpy
+
+mxd = arcpy.mapping.MapDocument(r"\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\Upper_Yaquina_TMDL_Blank_ME.mxd")
+df = arcpy.mapping.ListDataFrames(mxd, "Layers")[0]
+
+addLayer2 = arcpy.mapping.Layer(r"\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\UY_Elev.lyr")
+
+arcpy.mapping.AddLayer(df, addLayer2, "BOTTOM")
+
+mxd.saveACopy(r"\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\Upper_Yaquina_TMDL_UY_Elev.mxd")
+
+del mxd, addLayer2
+
